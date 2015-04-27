37f57cf83841021758a0d615ad326bb4739a9f69
Mayur Sharma <mayursharma@Mayurs-Air.wv.cc.cmu.edu>
2015-04-27 09:21:08 -0400

    Changed notifyProvider to make Markers work. Testing

--- UberErrands/static/js/notifyProvider.js
+++ UberErrands/static/js/notifyProvider.js
@@ -13,6 +13,17 @@ function sendRequest() {
     req.open("GET", "myRequestor?Username="+uname, false);
     req.send(); 
     document.getElementById("selectedOrNot").innerHTML=req.responseText;
+    var latp = document.getElementById("latR");
+    var lonp = document.getElementById("lonR");
+    var reqLatLng = new google.maps.LatLng(latp,lonp);
+    var marker2 = new google.maps.Marker({
+       position: reqLatLng,
+       draggable: true,
+       map: map,
+       labelAnchor: new google.maps.Point(22, 0),
+       labelStyle: {opacity: 0.75},
+       labelVisible: true
+     });
 }
 
 window.setInterval(sendRequest, 2000);
--- UberErrands/views/AddSubmitted.tpl
+++ UberErrands/views/AddSubmitted.tpl
@@ -30,10 +30,6 @@
 
     <!-- Custom Scripts -->
     <script src="http://maps.googleapis.com/maps/api/js"></script>
-    <script type="text/javascript">
-    var latp = {{.LatitudeR}};
-    var lonp = {{.LongitudeP}};
-    </script>
     <script src="/static/js/todo.js" type="text/javascript"></script>
     <script src="/static/js/notifyProvider.js" type="text/javascript"></script>
     <script src="/static/js/markerwithlabel.js" type="text/javascript"></script>
